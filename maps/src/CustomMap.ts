d711e75c3802452b1e5ef2863007acb0165bc8e3
cpustejovsky <charles.pustejovsky@gmail.com>
2020-07-04 15:18:08 -0400

    cleaning up code with interfaces

--- maps/src/CustomMap.ts
+++ maps/src/CustomMap.ts
@@ -1,12 +1,30 @@
+interface Entity {
+  location: {
+    lat: number;
+    lng: number;
+  };
+}
+
 export class CustomMap {
   private googleMap: google.maps.Map;
-  constructor(divId: string, lat: number, lng: number) {
+  constructor(divId: string, lat: number = 0, lng: number = 0) {
     this.googleMap = new google.maps.Map(document.getElementById(divId), {
-      zoom: 5,
+      zoom: 1,
       center: {
         lat: lat,
         lng: lng,
       },
     });
+  }
+
+  addMarker(Entity: Entity): void {
+    
+    new google.maps.Marker({
+      map: this.googleMap,
+      position: {
+        lat: Entity.location.lat,
+        lng: Entity.location.lng,
+      }
+    })
   }
 }
--- maps/src/index.ts
+++ maps/src/index.ts
@@ -1,7 +1,7 @@
 import { User } from "./User";
 import { Company } from "./Company";
 import { CustomMap } from "./CustomMap";
-// let user = new User();
-let company = new Company();
 
-const map = new CustomMap("map", company.location.lat, company.location.lng);
+const map = new CustomMap("map");
+map.addMarker(new User())
+map.addMarker(new Company())
