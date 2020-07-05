d93c09f1fca9993c79ce37df5c3f612ba7fafc16
cpustejovsky <charles.pustejovsky@gmail.com>
2020-07-05 12:10:14 -0400

    failing attempt

--- sortingAlgo/src/index.ts
+++ sortingAlgo/src/index.ts
@@ -1,7 +1,7 @@
 import {Sorter} from "./Sorter"
 import {NumbersCollection} from "./NumberCollection"
 import {CharactersCollection} from "./ChractersCollection"
-
+import {LinkedList} from "./LinkedList"
 const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
 const numSorter = new Sorter(numbersCollection)
 numSorter.sort();
@@ -11,3 +11,15 @@ const charactersCollection = new CharactersCollection("AZbdc");
 const charSorter = new Sorter(charactersCollection)
 charSorter.sort();
 console.log(charactersCollection.data);
+
+const linkedList = new LinkedList();
+linkedList.add(500);
+linkedList.add(250);
+linkedList.add(10);
+linkedList.add(800);
+linkedList.add(42);
+linkedList.add(5);
+
+const listSorter = new Sorter(linkedList)
+listSorter.sort()
+linkedList.print()
